43ceb5730a24abdc0fcf2db15a331d4e2e9f4499
Alex Richardson <alex@alexcr.com>
2025-12-01 09:15:40 -0500

    day 1

--- days/1.ts
+++ days/1.ts
@@ -1,12 +1,36 @@
 import { readFileSync } from "fs";
 
-const numbers = readFileSync('inputs/test.txt', 'utf8')
-  .split('\n')
-  .map(Number);
+const steps = readFileSync('inputs/1.txt', 'utf8').split('\n')
 
 export const day1 = () => {
-  return [
-    numbers.reduce((a, b) => a + b, 0),
-    numbers.reduce((a, b) => a + b, 0) + 1,
-  ];
+  let p1 = 0;
+  let p2 = 0;
+  let dial = 50;
+
+  const DIR: Record<string, number> = { 'L': -1, 'R': 1 };
+
+  for (const step of steps) {
+    const d = step[0];
+    const turn = Number(step.slice(1)) * DIR[d];
+    dial = (dial + turn) % 100;
+    if (dial === 0) p1++;
+  }
+
+  dial = 50;
+
+  for (const step of steps) {
+    const d = DIR[step[0]];
+    const turn = Number(step.slice(1));
+
+    for (let i = 0; i < turn; i++) {
+      dial += d;
+
+      if (dial < 0 || dial > 99) {
+        dial = ((dial % 100) + 100) % 100;
+      }
+      if (dial === 0) p2++;
+    }
+  }
+
+  return [p1, p2];
 };
